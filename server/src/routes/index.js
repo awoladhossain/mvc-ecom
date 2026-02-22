e1d48a6ce551286f5c8b297cfe150aecd53b5d51
awoladhossain <awoladh04@gmail.com>
2026-02-22 16:48:09 +0600

    coupon module added

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import authRoutes from "./auth.routes.js";
 import cartRoutes from "./cart.route.js";
+import couponRoutes from "./coupon.routes.js";
 import productRoutes from "./products.routes.js";
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/products", productRoutes);
 router.use("/cart", cartRoutes);
+router.use("/coupon", couponRoutes);
 
 export default router;
---
+++ server/src/services/coupon.service.js
@@ -0,0 +1,3 @@
+export const getCouponService = async()=>{
+  
+}
