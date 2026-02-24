eebc0c4799d9954ca1fbed84f027a310d37dfedb
awoladhossain <awoladh04@gmail.com>
2026-02-25 00:22:27 +0600

    payment module initial setup

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import authRoutes from "./auth.routes.js";
 import cartRoutes from "./cart.route.js";
 import couponRoutes from "./coupon.routes.js";
+import paymentRoutes from "./payment.routes.js";
 import productRoutes from "./products.routes.js";
+
 const router = express.Router();
+
 
 router.use("/auth", authRoutes);
 router.use("/products", productRoutes);
 router.use("/cart", cartRoutes);
 router.use("/coupon", couponRoutes);
+router.use("/payment", paymentRoutes);
 
 export default router;
