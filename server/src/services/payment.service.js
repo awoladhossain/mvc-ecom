770ab2fe720f5db3eda11bc7651c72a852f76cde
awoladhossain <awoladh04@gmail.com>
2026-03-03 23:30:20 +0600

    strip payment method intergration completed

--- server/src/services/payment.service.js
+++ server/src/services/payment.service.js
@@ -1,5 +1,6 @@
 import { stripe } from "../config/stripe.js";
 import Coupon from "../models/coupon.model.js";
+import Order from "../models/order.model.js";
 
 export const createCheckoutSessionService = async (payload, userId) => {
   const { products, couponCode } = payload;
@@ -8,9 +9,10 @@ export const createCheckoutSessionService = async (payload, userId) => {
   }
 
   // line item
-
+  let totalAmount = 0;
   const lineItems = products.map((product) => {
     const amount = Math.round(product.price * 100);
+    totalAmount += amount * product.quantity;
     return {
       price_data: {
         currency: "usd",
@@ -33,6 +35,11 @@ export const createCheckoutSessionService = async (payload, userId) => {
       userId: userId,
       isActive: true,
     });
+    if (coupon) {
+      totalAmount -= Math.round(
+        (totalAmount * coupon.discountPercentage) / 100,
+      );
+    }
   }
 
   // strip payment session
@@ -62,7 +69,49 @@ export const createCheckoutSessionService = async (payload, userId) => {
       ),
     },
   });
-  return session;
+  if (totalAmount > 10000) {
+    await createNewCoupon(userId);
+  }
+  const result = {
+    id: session.id,
+    totalAmount: totalAmount / 100,
+  };
+  return result;
+};
+
+export const checkoutSuccessService = async (params) => {
+  const { sessionId } = params;
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+  if (session.payment_status === "paid") {
+    if (session.metadata.couponCode) {
+      await Coupon.findOneAndUpdate(
+        {
+          code: session.metadata.couponCode,
+          userId: session.metadata.userId,
+        },
+        {
+          isActive: false,
+        },
+      );
+    }
+    // crate a new order
+    const products = JSON.parse(session.metadata.products);
+    const newOrder = new Order({
+      user: session.metadata.userId,
+      products: products.map((product) => ({
+        product: product.id,
+        quantity: product.quantity,
+        price: product.price,
+      })),
+      totalAmount: session.amount_total / 100,
+      stripeSessionId: sessionId,
+    });
+    await newOrder.save();
+
+    return newOrder._id;
+  }
 };
 
 // create stripe coupon
@@ -73,4 +122,18 @@ async function createStripeCoupon(discountPercentage) {
     duration: "once",
   });
   return stripeCoupon.id;
+}
+
+// create new coupon
+
+async function createNewCoupon(userId) {
+  await Coupon.findOneAndDelete({ userId });
+  const newCoupon = new Coupon({
+    code: "GIFT" + Math.random().toString(36).substring(2, 8).toUpperCase(),
+    discountPercentage: 10,
+    expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // expires in 30 days
+    userId: userId,
+  });
+  await newCoupon.save();
+  return newCoupon;
 }
